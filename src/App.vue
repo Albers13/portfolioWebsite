b541457f1a31f6f7cb2781a7214c399ca26c31b2
Albers13 <52482385+Albers13@users.noreply.github.com>
2023-05-04 22:45:47 -0500

    First commit for portfolio website
    Initial commit: Making sure everything is set up correctly

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+	<mainCard />
+</template>
+
+<script>
+import mainCard from './views/mainCard.vue'
+
+export default {
+	name: 'App',
+	components: {
+		mainCard
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
